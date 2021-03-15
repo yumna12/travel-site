c741eb4b349237c05930e751ba735792ec9b673b
yumna12 <78626244+yumna12@users.noreply.github.com>
2021-03-15 15:04:48 -0600

    completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,9 @@
 import '../styles/styles.css'
 import MobileMenu from './Modules/MobileMenu'
+import RevealOnScroll from './Modules/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 new MobileMenu();
 if(module.hot){
